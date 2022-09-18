dd2ffae8becc5b703c1a68ff08bf4b87f43c3b0d
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-18 12:01:06 +0200

    update version

--- utility/getCapital.tsx
+++ utility/getCapital.tsx
@@ -1,21 +1,30 @@
+interface DataCountryItem {
+  name: string;
+  capital: string;
+}
 
+type DataCountryArray = Array<DataCountryItem>;
 
-const getCapital = () => {
-    fetch("https://countriesnow.space/api/v0.1/countries/capital")
-        .then((res) => {
-            if (!res.ok) {
-                throw Error("Could not fetch data");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            setDataCountriesApi(data);
-        })
-        .catch((err) => {
-            console.log(err.message);
-            setFetchError(true);
-        });
+interface DataCountry {
+  error: boolean;
+  msg: string;
+  data: DataCountryArray;
+}
+
+export const getCapital = () => {
+  fetch("https://countriesnow.space/api/v0.1/countries/capital")
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Could not fetch data");
+      }
+      return res.json();
+    })
+    .then((data: DataCountry) => {
+      console.log("data from getCapital", data);
+      setDataCountriesApi(data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      setFetchError(true);
+    });
 };
-
-
-export default getCapital
--- utility/getWeather.tsx
+++ utility/getWeather.tsx
@@ -1,35 +1,40 @@
-const getWeather = (foundCapitalElement, dataCallback) => {
+interface WeatherItem {
+    main: string;
+  }
+  
+  interface DataWeather {
+    weather: WeatherItem[];
+  }
+
+export const getWeather = (
+    foundCapitalElement: string,
+    dataCallback: (weatherDetails: string[]) => void,
+
+  ) => {
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${foundCapitalElement}&appid=${process.env.NEXT_PUBLIC_WEATHERAPI}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${foundCapitalElement}&appid=${process.env.NEXT_PUBLIC_WEATHERAPI}`
     )
-        .then((res) => {
-            if (!res.ok) {
-                throw Error("Could not fetch data");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            console.log("weatherDataApi details: ", {
-                data,
-                dataName: data.name,
-                dataWeather: data.weather,
-            });
-            const weatherDetails = data.weather.map((detail) => {
-                return detail.main;
-            });
-            console.log("weatherDetails: ", weatherDetails);
-
-            dataCallback(weatherDetails);
-        })
-        .catch((err) => {
-            console.log("error", err);
-            setFetchError(true);
-        })
-        .finally(() => {
-            setIsLoading(false)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data");
+        }
+        return res.json();
+      })
+      .then((data: DataWeather) => {
+        console.log("weatherDataApi details fetch: ", data);
+        const weatherDetails = data.weather.map((detail) => {
+          return detail.main;
         });
-};
+        console.log("weatherDetails: ", weatherDetails);
 
-
-
-export default getWeather
+        dataCallback(weatherDetails);
+      })
+      .catch((err) => {
+        console.log("error", err);
+        setFetchError(true);
+      })
+      .finally(() => {
+        setIsSaveLoading(false);
+        setIsLoading(false);
+      });
+  };
